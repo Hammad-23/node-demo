33539752afac6537e45e56459d00739f5386fb60
Hammad-23 <hsyed2633@gmail.com>
2020-12-28 21:55:34 +0500

    use of middle ware in routes with express.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// use of middleware in routes
+
+// const express = require("express");
+// const app = express();
+// const checkUrl = function (req, res, next) {
+//   console.warn("current route is ", req.originalUrl);
+//   next();
+// };
+// app.use(checkUrl);
+// app.get("/", (req, res) => {
+//   res.send("This is Home");
+// });
+// app.get("/about", (req, res) => {
+//   res.send("This is about");
+// });
+// app.get("/login", (req, res) => {
+//   res.send("This is Login");
+// });
+// app.listen(8000);
+
+// use of middleware ends
